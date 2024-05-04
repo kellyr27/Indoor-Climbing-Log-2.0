a10a76d2cf253075f52981b4c7cb151cc0cbac1d
Kelly <83458976+kellyr27@users.noreply.github.com>
2024-05-04 12:12:00 +1000

    Removed server
    Migrated server to different project. Moving client to main folder.

--- server/tests/jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-
-module.exports = {
-    verbose: true,
-    testEnvironment: 'node',
-    setupFilesAfterEnv: ['./jest.setup.js'],
-    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
-    moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
-};
